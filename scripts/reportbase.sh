74db63e1fda1f063caad247c14bc4bc7903e433b
Jonatan Olofsson <jonatan.olofsson@gmail.com>
2011-04-17 01:57:17 +0200

    Bashification

--- scripts/reportbase.sh
+++ scripts/reportbase.sh
@@ -1,41 +1,64 @@
 #!/bin/bash
 
-SKELDIR = "/etc/reportbase/skel"
-INDEXFILE = "Report.tex"
-MKMATLAB = false
-MATLAB = "matlab -nojvm"
-GENERATE_SCRIPT = "/etc/reportbase/generate_report"
+SKELDIR="/etc/reportbase/skel"
+INDEXFILE="Report.tex"
+MKMATLAB=false
+MATLAB="matlab -nojvm"
+GENERATE_SCRIPT="generate_report_x"
 
 
 function make_chapter {
-  if [! -e "$1"]
+  if [ ! -e "$1" ]
   then
     mkdir "$1" "$1/figures"
-    cp $SKELDIR/chapter.tex "$1/$1.tex"
+    sed "s/::name::/$1/g" $SKELDIR/chapter.tex > "$1/$1.tex"
   fi
-  if $MKMATLAB
-  then
+  if [ $MKMATLAB -eq 1 ]; then
     mkdir "$1/matlab"
-    touch "$1/matlab/$1.m"
+    sed "s/::name::/$1/g" $SKELDIR/chapter.m > "$1/matlab/$1.m"
   fi
 }
 
 function register_chapter {
-  sed -i "s/%%::$TAG::%%/\newchapter{$1}\\n%%::$TAG::%%/"
+  sed -i "s/%%::$TAG::%%/\\\\newchapter{$1}\\n%%::$TAG::%%/" $INDEXFILE
 }
 
-if [ $# -le 2] ; then
-  echo "Not enough arguments."; exit
+function rename {
+  mv $1 $2
+  mv "$2/$1.tex" "$2/$2.tex"
+  if [ -d "$2/matlab" ] && [ -f "$2/matlab/$1.m" ] ; then
+    mv "$2/matlab/$1.m" "$2/matlab/$2.m"
+  fi
+}
+
+function help {
+  echo "Valid commands are: init, add [chapter(s)], generate and rename [old new]"
+  echo "Valid flags for the add command are"
+  echo " -a   Adds chapter as appendix"
+  echo " -m   Toggles between creating matlab directories or not. By default off"
+  echo ""
+  echo "reportbase is made by Jonatan Olofsson"
+}
+
+if [ ! -e $INDEXFILE ] && [ $1 != "init" ] ; then
+  echo "Indexfile $INDEXFILE could not be located. Either you're in the wrong directory, or it is not yet initialised"
 fi
 
+
+if [ $# -lt 1 ] ; then
+  echo "Not enough arguments."; help; exit
+fi
 
 case $1 in
   "init")
     cp -R $SKELDIR/{include,$INDEXFILE} .
     ;;
   "add")
-    TAG = "chapters"
-    counter = 0
+    if [ $# -le 1 ] ; then
+      echo "Not enough arguments."; help; exit
+    fi
+    TAG="chapters"
+    counter=0
     for arg in "$@"
     do
       counter=$(($counter+1))
@@ -44,10 +67,11 @@ case $1 in
       fi
       case $arg in
       "-a")
-        TAG = "appendices"
+        TAG="appendices"
+        MKMATLAB=false
         ;;
       "-m")
-        MKMATLAB = !$MKMATLAB
+        MKMATLAB=$(( ! $MKMATLAB ))
         ;;
       *)
         make_chapter $arg
@@ -57,7 +81,25 @@ case $1 in
     done
   ;;
   "generate")
-    $MATLAB -r $GENERATE_SCRIPT
+    $MATLAB -r "$GENERATE_SCRIPT"
+  ;;
+  "rename")
+    if [ ! $# -eq 3 ] ; then
+      echo "Wrong number of arguments"; help; exit
+    fi
+    if [ ! -d $2 ] ; then
+      echo "No such chapter: $2." ; exit
+    fi
+    if [ -d $3 ] ; then
+      echo "Chapter $3 exists." ; exit
+    fi
+
+    rename $2 $3
+
+    ;;
+  *)
+    echo "Invalid argument: $1"
+    help
   ;;
 esac
 
