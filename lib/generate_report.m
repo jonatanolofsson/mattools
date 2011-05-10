4c0b5d09ccce4368bc31e00399c036c77e0b35ad
Jonatan Olofsson <jonatan.olofsson@gmail.com>
2011-05-10 17:07:44 +0200

    Added GPL licensing information and README

--- lib/generate_report.m
+++ lib/generate_report.m
@@ -1,5 +1,36 @@
 %% Run all chapters m-files and generate all figures
+% 
+% #~ Copyright (C) 2011  Jonatan Olofsson
+% #~ This program is free software: you can redistribute it and/or modify
+% #~ it under the terms of the GNU General Public License as published by
+% #~ the Free Software Foundation, either version 3 of the License, or
+% #~ (at your option) any later version.
+% #~
+% #~ This program is distributed in the hope that it will be useful,
+% #~ but WITHOUT ANY WARRANTY; without even the implied warranty of
+% #~ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% #~ GNU General Public License for more details.
+% #~
+% #~ You should have received a copy of the GNU General Public License
+% #~ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+% 
+% #~ This software is provided by Jonatan Olofsson [jonatan.olofsson, gmail]
+%
 % Todo: Move show_plots to an option structure
+%
+%%% Input arguments:
+%	this            - The Figures object
+%
+%	dirs            - The name of the directory to generate (or a cell of names)
+%
+%	show_plots      - Whether to continue to show the plots after the
+%                       script has finished
+%
+%%% Credits and history
+% By:	Jonatan Olofsson			2010-11
+%
+% Changelog:
+%   [ none ]
 function generate_report(dirs, show_plots)
     if nargin < 1 || isempty(dirs)
         dirs = {};
@@ -10,7 +41,7 @@ function generate_report(dirs, show_plots)
     elseif ischar(dirs)
         dirs = {dirs};
     end
-    if nargin < 2, show_plots = 'off'; end;
+    if nargin < 2 || strcmpi(show_plots, 'On')~=0, show_plots = 'off'; end;
     kvars.show_plots = show_plots;
     kvars.dirs = dirs;
     
