9f9d263cbccc1aebfee4bd416fe09871d089811a
Jonatan Olofsson <jonatan.olofsson@gmail.com>
2011-05-21 18:20:19 +0200

    Variable name change

--- lib/generate_report.m
+++ lib/generate_report.m
@@ -41,22 +41,22 @@ function generate_report(dirs, show_plots)
     elseif ischar(dirs)
         dirs = {dirs};
     end
-    if nargin < 2 || strcmpi(show_plots, 'On')~=0, show_plots = 'off'; end;
+    if nargin < 2 || ~strcmpi(show_plots, 'On'), show_plots = 'off'; end;
     kvars.show_plots = show_plots;
     kvars.dirs = dirs;
     
-    for f = 1:length(kvars.dirs)
-        if strcmp(kvars.dirs{f}(1), '.'), continue; end;
-        if exist(kvars.dirs{f}, 'dir')
-            kvars.mdir = [kvars.dirs{f} '/matlab'];
+    for notjustsomeanyf = 1:length(kvars.dirs)
+        if strcmp(kvars.dirs{notjustsomeanyf}(1), '.'), continue; end;
+        if exist(kvars.dirs{notjustsomeanyf}, 'dir')
+            kvars.mdir = [kvars.dirs{notjustsomeanyf} '/matlab'];
             if exist(kvars.mdir, 'dir')
-                kvars.mfile = [kvars.mdir '/' kvars.dirs{f} '.m'];
+                kvars.mfile = [kvars.mdir '/' kvars.dirs{notjustsomeanyf} '.m'];
                 if exist(kvars.mfile, 'file')
                     figures = Figures();
                     figures.plots_visible = kvars.show_plots;
                     disp(kvars.mfile);
                     run(kvars.mfile);
-                    figures.save([kvars.dirs{f} '/figures']);
+                    figures.save([kvars.dirs{notjustsomeanyf} '/figures']);
                     if strcmp(kvars.show_plots, 'off')
                         figures.closeAll();
                     end
